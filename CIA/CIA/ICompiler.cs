0624fc0ee47e63c2be4d7c663664c6cc46b65e26
MyvarHD <myvarhd@gmail.com>
2015-04-18 12:04:13 +0200

    BasicScelitonFramework and parser engine

---
+++ CIA/CIA/ICompiler.cs
@@ -0,0 +1,21 @@
+﻿using CIA.Internals.Compiler.AST.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIA
+{
+    internal abstract class ICompiler
+    {
+        public void Compile(string Code, string OutPutFile, string Pattern)
+        {
+            GenCode(ParceScaned(ScanCode(Code, Pattern)), OutPutFile);
+        }
+
+        private string ScanCode(string code, string Pattern);// build a little string with tokens
+        private List<IAst> ParceScaned(string Tokens);// Convert tokens to ast
+        private void GenCode(List<IAst> ast, string path);// Convert ast to executble
+    }
+}
---
+++ CIA/CIA/Internals/Compiler/AST/Statments.cs
@@ -0,0 +1,13 @@
+﻿using CIA.Internals.Compiler.AST.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIA.Internals.Compiler.AST
+{
+    public  class Statments : IAst
+    {
+    }
+}
